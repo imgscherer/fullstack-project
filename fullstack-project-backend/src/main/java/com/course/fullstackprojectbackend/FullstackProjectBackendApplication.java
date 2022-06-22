ee059b82367fa8889c9d5747d974ede0caecedb6
Gabriel Scherer <gabrielscherer2709@gmail.com>
2022-06-21 21:54:35 -0300

    Backend project created

---
+++ fullstack-project-backend/src/main/java/com/course/fullstackprojectbackend/FullstackProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.course.fullstackprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackProjectBackendApplication.class, args);
+    }
+
+}
---
+++ fullstack-project-backend/src/test/java/com/course/fullstackprojectbackend/FullstackProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.fullstackprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
