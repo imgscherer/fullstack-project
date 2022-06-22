9d950b84ab839666cbc038c5a1b94dc6c60473fa
Gabriel Scherer <gabrielscherer2709@gmail.com>
2022-06-22 20:56:14 -0300

    Instantiation operation created

--- fullstack-project-backend/src/main/java/com/course/fullstackprojectbackend/FullstackProjectBackendApplication.java
+++ fullstack-project-backend/src/main/java/com/course/fullstackprojectbackend/FullstackProjectBackendApplication.java
@@ -1,13 +1,29 @@
 package com.course.fullstackprojectbackend;
 
+import com.course.fullstackprojectbackend.domain.Category;
+import com.course.fullstackprojectbackend.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class FullstackProjectBackendApplication {
+public class FullstackProjectBackendApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(FullstackProjectBackendApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Category cat1 = new Category(null, "Informática");
+        Category cat2 = new Category(null, "Escritório");
+
+        categoryRepository.saveAll(Arrays.asList(cat1,cat2));
+    }
 }
